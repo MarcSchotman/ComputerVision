6541f75b0b175cfa5b2910c456ae0bcd3ed0ab0d
SchotmanG <m_a_schotman@hotmail.com>
2017-06-05 10:40:27 +0200

    changed some pictures with paint.net, added the lecture slides, reorganized everything a bit

--- Code/ComputerVision.m
+++ Code/ComputerVision.m
@@ -1,57 +1,30 @@
-%Computer Vision = epic
+%read in images
 
 I1 = im2double(imread('C:\Users\Marc\Documents\GitHub\ComputerVision\Pictures\puzzle1.jpg'));
 I2 = im2double(imread('C:\Users\Marc\Documents\GitHub\ComputerVision\Pictures\partially_solved.jpg'));
 
-%% 
-
+%% harris corner detection
+disp('Detecting edges of the image(s)')
 
 [r1,c1] = harris(I1);
-
-%All 'edges' found inside this boundery layer (which follows the edge of the 
-%pics) will be deleted
-boundery = 10;
-
-%removes points found along the edges
-top_side = find(r1 < boundery);
-lower_side = find(r1 > length(I1(:,1))- boundery);
-left_side = find(c1 < boundery);
-right_side = find(c1 > length(I1) - boundery);
-
-%Alle lements that have to be deleted from both the row and corresponding column vector
-delete = [top_side; lower_side; left_side; right_side];
-
-%delete the rows and columns
-r1(delete) = [];
-c1(delete) = [];
+[r2,c2] = harris(I2);
 
 
-%plot image together with found edges
+%% Find individual pieces by grouping of found edges
+
+
+
+
+
+%% figures
+
+%plots images together with the 'edges' detected
 figure, imshow(I1,[]), hold on
-plot(c1,r1,'r+'), title('corners detected');
+plot(c1,r1,'r+'), title('edges detected');
 
 hold off
 
-%% second image (some error with deleteing of points around the edge for this image do not undestand :p
-
-[r2,c2] = harris(I2);
-
-boundery = 20;
-%delete points found along the edges
-
-top_side = find(r2 < boundery);
-lower_side = find(r2 > length(I2(:,1))- boundery);
-left_side = find(c2 < boundery);
-right_side = find(c2 > length(I2) - boundery);
-
-%put them in 1 matrix, thus all these elements have to eb deleted in
-%both the row and corresponding column vector
-delete = [top_side; lower_side; left_side; right_side];
-
-r2(delete) = [];
-c2(delete) = [];
-
 figure, imshow(I2,[]), hold on
-plot(c2,r2,'r+'), title('corners detected');
+plot(c2,r2,'r+'), title('edges detected');
 
 hold off
--- Code/harris.m
+++ Code/harris.m
@@ -43,7 +43,23 @@ function [r,c] = harris(I)
     alpha = 0.001;
     harris_im = lambda_1.*lambda_2 - alpha.*((lambda_1 + lambda_2).^2);
     radius = 1;
-    thresh = 0.0000002;
+    thresh = 0.000002;
   
 
     [r, c] = nonmaxsuppts(harris_im, radius, thresh, I);
+    
+    %All 'edges' found inside this boundery layer (which follows the edge of the 
+    %pics) will be deleted
+    boundery = 10;
+
+    %finds the corners detected within the boundery layer 
+    top_side = find(r < boundery);
+    lower_side = find(r > length(I(:,1))- boundery);
+    left_side = find(c < boundery);
+    right_side = find(c > length(I) - boundery);
+
+    %Deletes the found elements from the vector r and c
+    delete = [top_side; lower_side; left_side; right_side];
+    
+    r(delete) = [];
+    c(delete) = [];
